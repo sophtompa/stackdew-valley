d23415d93f8004a41b9d25097b55c6187649a5e1
slightly76 <slightly76@keyop.>
2025-04-23 16:18:41 +0100

    placeholder info and flavour text for dungeon

--- stackdew-test-repo/scenes/dungeonScene.js
+++ stackdew-test-repo/scenes/dungeonScene.js
@@ -6,12 +6,12 @@ import togglePause from '../src/togglePause.js';
 import { database, userInventory } from '../src/dummydata.js';
 
 export default class dungeonScene extends Phaser.Scene {
-    constructor() {
-        super('dungeonScene');
-    }
+	constructor() {
+		super('dungeonScene');
+	}
 
-    preload() {
-		this.load.image('dungeon', '../assets/newdungeon.png' )
+	preload() {
+		this.load.image('dungeon', '../assets/newdungeon.png');
 
 		this.load.spritesheet('playerSheet', '../assets/rose.png', {
 			frameWidth: 64,
@@ -23,44 +23,89 @@ export default class dungeonScene extends Phaser.Scene {
 		});
 	}
 
+	create() {
+		this.spaceKey = this.input.keyboard.addKey(
+			Phaser.Input.Keyboard.KeyCodes.SPACE
+		);
+		this.pKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+		//this.player = new Player(this, this.spawnX, this.spawnY, 'playerSheet');
 
-    create() {
-            this.spaceKey = this.input.keyboard.addKey(
-                Phaser.Input.Keyboard.KeyCodes.SPACE
-            );
-            this.pKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-            this.player = new Player(this, this.spawnX, this.spawnY, 'playerSheet');
+		// //enable keyboard
+		// this.input.keyboard.enabled = true;
 
-            // //enable keyboard
-		    // this.input.keyboard.enabled = true;
+		//scene fades in
+		this.cameras.main.fadeIn(1000, 0, 0, 0);
 
-		    //scene fades in
-		    this.cameras.main.fadeIn(1000, 0, 0, 0);
-    
-            //initialise dialogue manager
-            this.dialogue = new DialogueManager(this);
-            this.isDialogueRunning = false;
-    
-            // //initialise render inventory
-            // this.renderInventory = new renderInventory(this);
-            // this.renderInventory.render(userInventory);
+		//initialise dialogue manager
+		this.dialogue = new DialogueManager(this);
+		this.isDialogueRunning = false;
 
-            //create new map
-		    this.add.image(400, 221, 'dungeon')
-        }
+		// //initialise render inventory
+		this.renderInventory = new renderInventory(this);
+		this.renderInventory.render(userInventory);
 
-        update() {
-            if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-                this.moveScene();
-            }
-        }
+		//create new map
+		this.add.image(400, 221, 'dungeon');
 
-        moveScene() {
-            this.input.keyboard.enabled = false;
-            this.cameras.main.fadeOut(1000, 0, 0, 0);
-            this.time.delayedCall(1000, () => {
-                this.scene.start('overworldScene', { from: 'dungeonScene' });
-                this.input.keyboard.enabled = true;
-            });
-        }
+		this.dialogue.startDialogue(
+			[
+				{
+					text: `"Wicked Mitch of the West's Tech Dungeon"(tm) is not safe to be visited right now.`,
+					speaker: '',
+					color: '#1f451c',
+				},
+				{
+					text: `Please come back at a later date to see how your Devlings have suffered.`,
+					speaker: '',
+					color: '#1f451c',
+				},
+				{
+					text: `An infinite loop of...`,
+					speaker: 'left',
+					color: '#1f451c',
+					x: 330,
+					y: 135,
+				},
+				{
+					text: `PAIN!!!`,
+					speaker: 'left',
+					color: '#1f451c',
+					x: 330,
+					y: 135,
+				},
+				{
+					text: `Trapped in development hell!`,
+					speaker: 'left',
+					color: '#1f451c',
+					x: 460,
+					y: 165,
+				},
+				{
+					text: `Thanks CorthNoders!`,
+					speaker: 'left',
+					color: '#1f451c',
+					x: 395,
+					y: 60,
+				},
+			],
+			null,
+			220,
+			365
+		);
+	}
+
+	update() {
+		if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+			this.moveScene();
+		}
+	}
+
+	moveScene() {
+		this.input.keyboard.enabled = false;
+		this.cameras.main.fadeOut(1000, 0, 0, 0);
+		this.time.delayedCall(1000, () => {
+			this.scene.start('overworldScene', { from: 'dungeonScene' });
+			this.input.keyboard.enabled = true;
+		});
+	}
 }
